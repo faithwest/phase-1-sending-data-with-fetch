05ec9105bcfd74a90546f9ea5a8f4cb7b68fd16e
faithwest <faithchepkoech425@gmail.com>
2023-10-06 15:55:57 +0300

    1st mile

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+let name = "Steve"
+let email = "steve@steve.com"
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({    
+    userName = "Steve"
+    userEmail = "steve@steve.com"
+
+    }),
+  };
